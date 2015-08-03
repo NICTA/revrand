f6b77f6391085d1e7009845ff9d374cf4a18f5a3
Daniel Steinberg <dan.m.steinberg@gmail.com>
2015-08-03 10:46:59 +1000

    reduced version number required for scipy

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@
 from setuptools import setup
 
 setup(
-    name = 'pyalacarte',
-    version = '0.1',
-    description = 'Implementation of the A la Carte approximation of Gaussian'
-                  'processes, amongst other things.',
-    author = 'Daniel Steinberg',
-    author_email = 'daniel.steinberg@nicta.com.au',
+    name='pyalacarte',
+    version='0.1',
+    description='Implementation of the A la Carte approximation of Gaussian'
+                'processes, amongst other things.',
+    author='Daniel Steinberg',
+    author_email='daniel.steinberg@nicta.com.au',
     url='',
-    packages = ['pyalacarte'],
-    install_requires = [
-        'scipy >= 0.15.0',  # These may be able to be lower, just not tested
+    packages=['pyalacarte'],
+    install_requires=[
+        'scipy >= 0.14.1',
         'numpy >= 1.8.2',
         # NLopt >= 2.4.2
     ]
