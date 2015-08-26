4e971c86d7f74417eac574cc96c8bac82a98d909
Daniel Steinberg <dan.m.steinberg@gmail.com>
2015-08-26 19:55:21 +1000

    added test dep

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+import numpy as np
+
+
+@pytest.fixture
+def make_quadratic():
+
+    # Find quadratic parameters y = a*x**2 + b*x + c
+    a = 3.
+    b = 2.
+    c = 1.
+    N = 1000
+
+    bounds = [(None, None), (None, None), (1.1, None)]
+
+    x = np.linspace(-1, 1, N)
+    y = a*x**2 + b*x + c
+    data = np.vstack((y, x)).T
+
+    return a, b, c, data, bounds
