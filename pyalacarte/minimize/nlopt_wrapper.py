43577c3a4e81199a8c048002800a72474223fe8b
Louis Tiao <louistiao@gmail.com>
2015-09-21 12:26:10 +1000

    nlopt wrap

--- pyalacarte/minimize/nlopt_wrapper.py
+++ pyalacarte/minimize/nlopt_wrapper.py
@@ -1,27 +1,48 @@
 import numpy as np
 import nlopt
 
+from pyalacarte.utils import couple
+
+from scipy.optimize import OptimizeResult
 from six.moves import filter, range
 from functools import partial
 from warnings import warn
 from re import search
 
-# NLOpt Helper
 NLOPT_ALGORITHMS_KEYS = list(filter(partial(search, r'^[GL][ND]_'), dir(nlopt)))
+
 NLOPT_ALGORITHMS = {k:getattr(nlopt, k) for k in NLOPT_ALGORITHMS_KEYS}
 
-class NLOptimize(nlopt.opt):
+NLOPT_MESSAGES = {
+    nlopt.SUCCESS: 'Success',
+    nlopt.STOPVAL_REACHED: 'Optimization stopped because stopval (above) '
+                           'was reached.',
+    nlopt.FTOL_REACHED: 'Optimization stopped because ftol_rel or ftol_abs '
+                        '(above) was reached.',
+    nlopt.XTOL_REACHED: 'Optimization stopped because xtol_rel or xtol_abs ' 
+                        '(above) was reached.',
+    nlopt.MAXEVAL_REACHED: 'Optimization stopped because maxeval (above) '
+                           'was reached.',
+    nlopt.MAXTIME_REACHED: 'Optimization stopped because maxtime (above) ' 
+                           'was reached.',
+    nlopt.FAILURE: 'Failure',
+    nlopt.INVALID_ARGS: 'Invalid arguments (e.g. lower bounds are bigger '
+                        'than upper bounds, an unknown algorithm was '
+                        'specified, etcetera).',
+    nlopt.OUT_OF_MEMORY: 'Ran out of memory.',
+    nlopt.ROUNDOFF_LIMITED: 'Halted because roundoff errors limited progress. '
+                            '(In this case, the optimization still typically '
+                            'returns a useful result.)',
+    nlopt.FORCED_STOP: "Halted because of a forced termination: the user " 
+                       "called nlopt_force_stop(opt) on the optimization's "
+                       "nlopt_opt object opt from the user’s objective "
+                       "function or constraints."
+}
 
-    def __init__():
-
-    def __call__(x0=None):
-
-        self.optimize(x0)
-
-def get_nlopt_by_name(method_name=None):
+def get_nlopt_enum_by_name(method_name=None):
     """
-    Get NLOpt algorithm by name. If the algorithm is not found, defaults
-    to `nlopt.LN_BOBYQA`.
+    Get NLOpt algorithm object by name. If the algorithm is not found, 
+    defaults to `nlopt.LN_BOBYQA`.
 
     Notes
     -----
@@ -51,35 +72,37 @@ def get_nlopt_by_name(method_name=None):
 
     Examples
     --------
-    >>> get_nlopt_by_name('LN_NELDERMEAD') == nlopt.LN_NELDERMEAD
+    >>> get_nlopt_enum_by_name('LN_NELDERMEAD') == nlopt.LN_NELDERMEAD
     True
 
-    >>> get_nlopt_by_name('ln_neldermead') == nlopt.LN_NELDERMEAD
+    >>> get_nlopt_enum_by_name('ln_neldermead') == nlopt.LN_NELDERMEAD
     True
 
     One is permitted to be cavalier with these method names.
 
-    >>> get_nlopt_by_name('ln_NelderMead') == nlopt.LN_NELDERMEAD
+    >>> get_nlopt_enum_by_name('ln_NelderMead') == nlopt.LN_NELDERMEAD
     True
 
-    >>> get_nlopt_by_name() == nlopt.LN_BOBYQA
+    >>> get_nlopt_enum_by_name() == nlopt.LN_BOBYQA
     True
 
-    >>> get_nlopt_by_name('foobar') == nlopt.LN_BOBYQA
+    >>> get_nlopt_enum_by_name('foobar') == nlopt.LN_BOBYQA
     True
 
     .. todo:: Exceptional cases (low-priority)
 
-    >>> get_nlopt_by_name('G_MLSL') == nlopt.G_MLSL # doctest: +SKIP
+    >>> get_nlopt_enum_by_name('G_MLSL') == nlopt.G_MLSL # doctest: +SKIP
     True
 
-    >>> get_nlopt_by_name('AUGLAG') == nlopt.AUGLAG # doctest: +SKIP
+    >>> get_nlopt_enum_by_name('AUGLAG') == nlopt.AUGLAG # doctest: +SKIP
     True
     """
 
     return NLOPT_ALGORITHMS.get(method_name.upper() if method_name is not None \
         else None, nlopt.LN_BOBYQA)
 
+def get_nlopt_by_name(method_name=None, dimensions=None):
+    pass
 
 def make_nlopt_obj(fun, args=(), jac=True):
 
@@ -224,48 +247,118 @@ def make_nlopt_obj(fun, args=(), jac=True):
                     if grad_temp is None:
                         warn('Using gradient-based optimization with '
                             'jac=True, but no gradient information is '
-                            'provided.', RuntimeWarning)
+                            'available.', RuntimeWarning)
                     else:
                         grad[:] = grad_temp
                 else:
                     if grad_temp is not None:
                         warn('Using gradient-based optimization with '
-                            'jac=True, the provided gradient information '
+                            'jac=False, the provided gradient information '
                             'is ignored.', RuntimeWarning)
 
         return val
 
     return nlopt_obj_fun
 
-def nlopt_minimize(fun, x0, args=(), method=None, jac=None, bounds=None, 
-                   constraints=(), ftol=1e-5, xtol=1e-5, maxiter=None):
+def normalize_bound(bound):
+    """
+    >>> normalize_bound((2.6, 7.2))
+    (2.6, 7.2)
 
-    obj_fun = make_nlopt_obj(fun, args, jac)
+    >>> normalize_bound((None, 7.2))
+    (-inf, 7.2)
 
-    dim = len(x0)
+    >>> normalize_bound((2.6, None))
+    (2.6, inf)
+
+    >>> normalize_bound((None, None))
+    (-inf, inf)
+
+    This operation is idempotent
+    >>> normalize_bound((-float("inf"), float("inf")))
+    (-inf, inf)
+    """
+    min_, max_ = bound
+    
+    if min_ is None:
+        min_ = -float('inf')
+    
+    if max_ is None:
+        max_ = float('inf')
+
+    return min_, max_
+
+def normalize_bounds(bounds=[]):
+    """
+    >>> bounds = [(2.6, 7.2), (None, 2), (3.14, None), (None, None)]
+    >>> list(normalize_bounds(bounds))
+    [(2.6, 7.2), (-inf, 2), (3.14, inf), (-inf, inf)]
+    """
+    return map(normalize_bound, bounds)
+
+def make_nlopt_message(ret_code):
+    """
+    >>> make_nlopt_message(nlopt.SUCCESS)
+    'Success'
+    
+    >>> make_nlopt_message(nlopt.INVALID_ARGS)
+    'Invalid arguments (e.g. lower bounds are bigger than upper bounds, an unknown algorithm was specified, etcetera).'
+    """ 
+    return NLOPT_MESSAGES.get(ret_code)
+
+def nlopt_minimize(fun, x0, args=(), method=None, jac=None, bounds=[], 
+                   constraints=[], ftol=None, xtol=None, maxiter=None):
+    """
+    >>> from scipy.optimize import rosen, rosen_der
+    >>> x0 = [1.3, 0.7, 0.8, 1.9, 1.2]
+    
+    >>> res = nlopt_minimize(rosen, x0, method='ld_lbfgs', jac=rosen_der)
+    >>> res.success
+    True
+    >>> res.message
+    'Success'
+    >>> np.isclose(res.fun, 0)
+    True
+    >>> res.x
+    array([ 1.,  1.,  1.,  1.,  1.])
+
+    >>> res = nlopt_minimize(rosen, x0, method='cobyla', constraints=[3])
+    """
+    if constraints:
+        warn("Equality and inequality constraints are currently "
+             "not supported", NotImplementedError)
+
     # Create NLopt object
-    N = 
-    opt = nlopt.opt(method, N)
-    opt.set_min_objective(obj)
+    dim = len(x0)
 
-    # Translate the parameter bounds
+    if isinstance(method, str):
+        method = get_nlopt_enum_by_name(method)
+
+    opt = nlopt.opt(method, dim)
+
+    # Create NLOpt objective function
+    obj_fun = make_nlopt_obj(fun, args, jac)
+    opt.set_min_objective(obj_fun)
+
+    # Normalize and set parameter bounds
     if bounds:
-        lower = [b[0] if b[0] else -float('inf') for b in bounds]
-        upper = [b[1] if b[1] else float('inf') for b in bounds]
+        lower, upper = zip(*normalize_bounds(bounds))
         opt.set_lower_bounds(lower)
         opt.set_upper_bounds(upper)
 
-    # Stoping Criteria
-    if ftol:
+    # Termination Criteria
+    if ftol is not None:
         opt.set_ftol_rel(ftol)
-    if xtol:
+
+    if xtol is not None:
         opt.set_xtol_rel(xtol)
-    if maxiter:
+
+    if maxiter is not None:
         opt.set_maxeval(maxiter)
 
     return OptimizeResult(
         x = opt.optimize(x0), 
         fun = opt.last_optimum_value(),
-        message = str(opt.last_optimize_result()),
+        message = make_nlopt_message(opt.last_optimize_result()),
         success = opt.last_optimize_result() > 0,
     )
