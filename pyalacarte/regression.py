e1a4fa8d9072f53fd5c33fc75e842b339a544d3a
Daniel Steinberg <dan.m.steinberg@gmail.com>
2015-10-25 22:46:06 +1100

    small update to correct reg doco

--- pyalacarte/regression.py
+++ pyalacarte/regression.py
@@ -227,7 +227,7 @@ def bayeslinear_sgd(X, y, basis, bparams, var=1, regulariser=1., rank=None,
             SGD decay rate, must be [0, 1].
         batchsize: int, optional
             number of observations to use per SGD batch.
-        verbose: bool,
+        verbose: bool, optional
             log the learning status.
 
     Returns
