1fbaabf973580bc44c989e0ff2fa4c3505b94596
dsteinberg <dan.m.steinberg@gmail.com>
2015-11-19 14:44:17 +1100

    merged some changes in from glm branch

--- revrand/classification.py
+++ revrand/classification.py
@@ -6,6 +6,7 @@ import numpy as np
 import logging
 
 from .optimize import minimize, sgd
+from .transforms import softmax
 
 
 # Set up logging
@@ -155,57 +156,12 @@ def logistic_predict(X_star, weights, basis, bparams):
             the `label` order output by the learning function.
     """
 
-    return _softmax(basis(X_star, *bparams).dot(weights), axis=1)
+    return softmax(basis(X_star, *bparams).dot(weights), axis=1)
 
 
 #
 # Private module functions
 #
-
-def _logistic(X):
-    """ Pass X through a logistic sigmoid, 1 / (1 + exp(-X)), in a numerically
-        stable way (using the log-sum-exp trick).
-
-        Arguments:
-            X: shape (N,) array or shape (N, D) array of data.
-
-        Returns:
-            array of same shape of X with the result of logistic(X).
-    """
-
-    N = X.shape[0]
-
-    if X.ndim == 1:
-        return np.exp(-_logsumexp(np.vstack((np.zeros(N), -X)).T, axis=1))
-    elif X.ndim == 2:
-        lgX = np.empty(X.shape, dtype=float)
-        for d in range(X.shape[1]):
-            lgX[:, d] = np.exp(-_logsumexp(np.vstack((np.zeros(N),
-                                                      -X[:, d])).T, axis=1))
-        return lgX
-    else:
-        raise ValueError("This only works on up to 2D arrays.")
-
-
-def _softmax(X, axis=0):
-    """ Pass X through a softmax function, exp(X) / sum(exp(X), axis=axis), in
-        a numerically stable way using the log-sum-exp trick.
-    """
-
-    if axis == 1:
-        return np.exp(X - _logsumexp(X, axis=1)[:, np.newaxis])
-    elif axis == 0:
-        return np.exp(X - _logsumexp(X, axis=0))
-    else:
-        raise ValueError("This only works on 2D arrays for now.")
-
-
-def _logsumexp(X, axis=0):
-    """ Log-sum-exp trick for matrix X for summation along a specified axis """
-
-    mx = X.max(axis=axis)
-    return np.log(np.exp(X - mx[:, np.newaxis]).sum(axis=axis)) + mx
-
 
 def _MAP(weights, data, regulariser, cweights, verbose, N=None):
     y, Phi = data[:, 0], data[:, 1:]
@@ -216,7 +172,7 @@ def _MAP(weights, data, regulariser, cweights, verbose, N=None):
     if cweights is None:
         cweights = np.ones(K)
 
-    sig = _softmax(Phi.dot(weights), axis=1)
+    sig = softmax(Phi.dot(weights), axis=1)
     grad = np.zeros_like(weights)
     MAP = 0
 
