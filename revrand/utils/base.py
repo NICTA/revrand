83f09151ae64e1375c9bdb8b2e6739cb32a22bea
Daniel Steinberg <dan.m.steinberg@gmail.com>
2015-12-17 14:07:27 +1100

    incorporated dave's pickling fix

--- revrand/utils/base.py
+++ revrand/utils/base.py
@@ -120,7 +120,7 @@ class Positive(Bound):
         ...
     ValueError: lower bound must be positive!
     """
-    def __new__(cls, lower=1e-14, *args):
+    def __new__(cls, lower=1e-14):
 
         if lower <= 0:
             raise ValueError('lower bound must be positive!')
