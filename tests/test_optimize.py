eafdd7f5ae8f462ac3aa152709c6230d9f02b10e
Daniel Steinberg <dan.m.steinberg@gmail.com>
2016-01-27 16:34:52 +1100

    added optional nlopt testing if it is present

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -1,10 +1,18 @@
 from __future__ import division
+from warnings import warn
 
 import numpy as np
 
 from revrand.optimize import sgd, minimize, structured_minimizer, \
     logtrick_minimizer, structured_sgd, logtrick_sgd, Bound, Positive
 from revrand.utils import flatten
+
+try:
+    import nlopt
+    nlopt_test = True
+except ImportError:
+    warn('NLopt could not be imported, not testing nlopt routines.')
+    nlopt_test = False
 
 
 def test_unbounded(make_quadratic):
@@ -20,6 +28,12 @@ def test_unbounded(make_quadratic):
     res = minimize(qobj, w0, args=(data,), jac=True, method='L-BFGS-B')
     Ea, Eb, Ec = res['x']
     assert np.allclose((a, b, c), (Ea, Eb, Ec), atol=1e-3, rtol=0)
+
+    if nlopt_test:
+        res = minimize(qobj, w0, args=(data, False), jac=False,
+                       method='LN_BOBYQA', backend='nlopt')
+        Ea, Eb, Ec = res['x']
+        assert np.allclose((a, b, c), (Ea, Eb, Ec), atol=1e-3, rtol=0)
 
     res = minimize(qobj, w0, args=(data, False), jac=False, method=None)
     Ea, Eb, Ec = res['x']
@@ -43,6 +57,13 @@ def test_bounded(make_quadratic):
                        (Ea_sgd, Eb_sgd, Ec_sgd),
                        atol=1e-2, rtol=0)
 
+    if nlopt_test:
+        res = minimize(qobj, w0, args=(data, False), jac=False, bounds=bounds,
+                       method='LN_BOBYQA', backend='nlopt')
+        Ea_bq, Eb_bq, Ec_bq = res['x']
+        assert np.allclose((Ea_bfgs, Eb_bfgs, Ec_bfgs),
+                           (Ea_bq, Eb_bq, Ec_bq), atol=1e-3, rtol=0)
+
 
 def test_structured_params(make_quadratic):
 
@@ -54,14 +75,22 @@ def test_structured_params(make_quadratic):
                method='L-BFGS-B')
     (Ea_bfgs, Eb_bfgs), Ec_bfgs = res['x']
 
+    assert np.allclose((Ea_bfgs, Eb_bfgs, Ec_bfgs), (a, b, c), atol=1e-3,
+                       rtol=0)
+
     nsgd = structured_sgd(sgd)
     res = nsgd(qobj_struc, w0, data, bounds=None, eval_obj=True, gtol=1e-4,
                passes=1000, rate=0.95, eta=1e-6)
     (Ea_sgd, Eb_sgd), Ec_sgd = res['x']
 
-    assert np.allclose((Ea_bfgs, Eb_bfgs, Ec_bfgs), (a, b, c), atol=1e-2,
-                       rtol=0)
     assert np.allclose((Ea_sgd, Eb_sgd, Ec_sgd), (a, b, c), atol=1e-1, rtol=0)
+
+    if nlopt_test:
+        res = nmin(qobj_struc, w0, args=(data, False), jac=False, bounds=None,
+                   method='LN_BOBYQA', backend='nlopt')
+        (Ea_bq, Eb_bq), Ec_bq = res['x']
+
+        assert np.allclose((Ea_bq, Eb_bq, Ec_bq), (a, b, c), atol=1e-3, rtol=0)
 
 
 def test_log_params(make_quadratic):
@@ -75,14 +104,22 @@ def test_log_params(make_quadratic):
                method='L-BFGS-B')
     Ea_bfgs, Eb_bfgs, Ec_bfgs = res['x']
 
+    assert np.allclose((Ea_bfgs, Eb_bfgs, Ec_bfgs), (a, b, c), atol=1e-3,
+                       rtol=0)
+
     nsgd = logtrick_sgd(sgd)
     res = nsgd(qobj, w0, data, bounds=bounds, eval_obj=True, gtol=1e-4,
                passes=1000, rate=0.95, eta=1e-6)
     Ea_sgd, Eb_sgd, Ec_sgd = res['x']
 
-    assert np.allclose((Ea_bfgs, Eb_bfgs, Ec_bfgs), (a, b, c), atol=1e-2,
-                       rtol=0)
     assert np.allclose((Ea_sgd, Eb_sgd, Ec_sgd), (a, b, c), atol=1e-1, rtol=0)
+
+    if nlopt_test:
+        res = nmin(qobj, w0, args=(data, False), jac=False, bounds=bounds,
+                   method='LN_BOBYQA', backend='nlopt')
+        Ea_bq, Eb_bq, Ec_bq = res['x']
+
+        assert np.allclose((Ea_bq, Eb_bq, Ec_bq), (a, b, c), atol=1e-3, rtol=0)
 
 
 def test_logstruc_params(make_quadratic):
@@ -96,14 +133,22 @@ def test_logstruc_params(make_quadratic):
                method='L-BFGS-B')
     (Ea_bfgs, Eb_bfgs), Ec_bfgs = res['x']
 
+    assert np.allclose((Ea_bfgs, Eb_bfgs, Ec_bfgs), (a, b, c), atol=1e-2,
+                       rtol=0)
+
     nsgd = structured_sgd(logtrick_sgd(sgd))
     res = nsgd(qobj_struc, w0, data, bounds=bounds, eval_obj=True, gtol=1e-4,
                passes=1000, rate=0.95, eta=1e-6)
     (Ea_sgd, Eb_sgd), Ec_sgd = res['x']
 
-    assert np.allclose((Ea_bfgs, Eb_bfgs, Ec_bfgs), (a, b, c), atol=1e-2,
-                       rtol=0)
     assert np.allclose((Ea_sgd, Eb_sgd, Ec_sgd), (a, b, c), atol=1e-1, rtol=0)
+
+    if nlopt_test:
+        res = nmin(qobj_struc, w0, args=(data, False), jac=False, bounds=None,
+                   method='LN_BOBYQA', backend='nlopt')
+        (Ea_bq, Eb_bq), Ec_bq = res['x']
+
+        assert np.allclose((Ea_bq, Eb_bq, Ec_bq), (a, b, c), atol=1e-3, rtol=0)
 
 
 def qobj(w, data, grad=True):
