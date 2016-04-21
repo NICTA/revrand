added more to report, added notebook, fixed many numerical errors and refactored transforms

demos/demo_glm.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as pl
 import numpy as np
 import logging
 from scipy.stats import poisson, bernoulli
+from scipy.special import expit
 
-from revrand import basis_functions, glm, likelihoods, transforms
+from revrand import basis_functions, glm, likelihoods
 from revrand.utils.datasets import gen_gausprocess_se
+from revrand.utils import softplus
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -54,13 +56,13 @@ Xtrain, ytrain, Xtest, ftest = \
 
 if like == 'Bernoulli':
 
-    ytrain = bernoulli.rvs(transforms.logistic(20 * ytrain))
-    ftest = transforms.logistic(20 * ftest)
+    ytrain = bernoulli.rvs(expit(20 * ytrain))
+    ftest = expit(20 * ftest)
 
 elif like == 'Poisson':
 
-    ytrain = poisson.rvs(transforms.softplus(5 * ytrain))
-    ftest = transforms.softplus(5 * ftest)
+    ytrain = poisson.rvs(softplus(5 * ytrain))
+    ftest = softplus(5 * ftest)
 
 #
 # Make Bases and Likelihood

revrand/basis_functions/base.py
@@ -16,6 +16,7 @@ from scipy.spatial.distance import cdist
 
 from ..optimize import Positive
 from ..linalg import hadamard
+from ..utils import safediv
 
 # TODO:
 # - Remove the need to bases to know their params bounds, see #54 and #55
@@ -411,7 +412,8 @@ class RadialBasis(Basis):
         if self.D != D:
             raise ValueError("X has inconsistent dimensionality!")
 
-        return np.exp(- cdist(X, self.C, 'sqeuclidean') / (2 * lenscale**2))
+        return np.exp(- safediv(cdist(X, self.C, 'sqeuclidean'),
+                                (2 * lenscale**2)))
 
     @slice_call
     def grad(self, X, lenscale):
@@ -434,7 +436,8 @@ class RadialBasis(Basis):
             raise ValueError("X has inconsistent dimensionality!")
 
         sdist = cdist(X, self.C, 'sqeuclidean')
-        dPhi = np.exp(- sdist / (2 * lenscale**2)) * sdist / lenscale**3
+        dPhi = np.exp(- safediv(sdist, 2 * lenscale**2)) \
+            * safediv(sdist, lenscale**3)
 
         return dPhi
 
@@ -489,7 +492,7 @@ class SigmoidalBasis(Basis):
             raise ValueError("Expected X of dimensionality {0}, got {1}"
                              .format(self.D, D))
 
-        return expit(cdist(X, self.C, 'seuclidean') / lenscale)
+        return expit(safediv(cdist(X, self.C, 'seuclidean'), lenscale))
 
     @slice_call
     def grad(self, X, lenscale):
@@ -521,9 +524,9 @@ class SigmoidalBasis(Basis):
 
         dist = cdist(X, self.C, 'seuclidean')
 
-        sigma = expit(dist / lenscale)
+        sigma = expit(safediv(dist, lenscale))
 
-        dPhi = - dist * sigma * (1 - sigma) / lenscale**2
+        dPhi = - dist * sigma * safediv((1 - sigma), lenscale**2)
 
         return dPhi
 
@@ -572,7 +575,7 @@ class RandomRBF(RadialBasis):
         N, D = X.shape
         self._checkD(D)
 
-        WX = np.dot(X, self.W / lenscale)
+        WX = np.dot(X, safediv(self.W, lenscale))
 
         return np.hstack((np.cos(WX), np.sin(WX))) / np.sqrt(self.n)
 
@@ -595,8 +598,8 @@ class RandomRBF(RadialBasis):
         N, D = X.shape
         self._checkD(D)
 
-        WX = np.dot(X, self.W / lenscale)
-        dWX = WX / lenscale
+        WX = np.dot(X, safediv(self.W, lenscale))
+        dWX = safediv(WX, lenscale)
 
         return np.hstack((dWX * np.sin(WX), -dWX * np.cos(WX))) \
             / np.sqrt(self.n)
@@ -647,7 +650,7 @@ class RandomRBF_ARD(RandomRBF):
         N, D = X.shape
         self._checkD(D, len(lenscales))
 
-        WX = np.dot(X, self.W / np.asarray(lenscales)[:, np.newaxis])
+        WX = np.dot(X, safediv(self.W, lenscales[:, np.newaxis]))
 
         return np.hstack((np.cos(WX), np.sin(WX))) / np.sqrt(self.n)
 
@@ -670,13 +673,13 @@ class RandomRBF_ARD(RandomRBF):
         N, D = X.shape
         self._checkD(D, len(lenscales))
 
-        WX = np.dot(X, self.W / np.asarray(lenscales)[:, np.newaxis])
+        WX = np.dot(X, safediv(self.W, lenscales[:, np.newaxis]))
         sinWX = - np.sin(WX)
         cosWX = np.cos(WX)
 
         dPhi = []
         for i, l in enumerate(lenscales):
-            dWX = np.outer(X[:, i], - 1. / l**2 * self.W[i, :])
+            dWX = np.outer(X[:, i], - safediv(self.W[i, :], l**2))
             dPhi.append(np.hstack((dWX * sinWX, dWX * cosWX))
                         / np.sqrt(self.n))
 
@@ -746,7 +749,7 @@ class FastFood(RandomRBF):
 
         self._checkD(X.shape[1])
 
-        VX = self.__makeVX(X) / lenscale
+        VX = safediv(self.__makeVX(X), lenscale)
         Phi = np.hstack((np.cos(VX), np.sin(VX))) / np.sqrt(self.n)
         return Phi
 
@@ -769,8 +772,8 @@ class FastFood(RandomRBF):
 
         self._checkD(X.shape[1])
 
-        VX = self.__makeVX(X) / lenscale
-        dVX = - VX / lenscale
+        VX = safediv(self.__makeVX(X), lenscale)
+        dVX = - safediv(VX, lenscale)
 
         return np.hstack((-dVX * np.sin(VX), dVX * np.cos(VX))) \
             / np.sqrt(self.n)

revrand/glm.py
@@ -15,9 +15,8 @@ from multiprocessing import Pool
 from scipy.stats.distributions import gamma
 from scipy.optimize import brentq, minimize
 
-from .utils import couple, append_or_extend, atleast_list
+from .utils import couple, append_or_extend, atleast_list, logsumexp, safediv
 from .basis_functions import apply_grad
-from .transforms import logsumexp
 from .optimize import sgd, structured_sgd, structured_minimizer, logtrick_sgd,\
     logtrick_minimizer, Bound, Positive, AdaDelta
 
@@ -143,6 +142,7 @@ def learn(X, y, likelihood, lparams, basis, bparams, regulariser=1.,
         # Extract data, parameters, etc
         _lpars, _bpars, = args[:nlpams], args[nlpams:(nlpams + nbpams)]
         y, X = args[-1][:, 0], args[-1][:, 1:]
+        ireg = safediv(1., _reg)
 
         # Dimensions
         M, d = X.shape
@@ -172,7 +172,7 @@ def learn(X, y, likelihood, lparams, basis, bparams, regulariser=1.,
             df[:, k] = B * likelihood.df(y, f[:, k], *_lpars)
             d2f[:, k] = B * likelihood.d2f(y, f[:, k], *_lpars)
             d3f[:, k] = B * likelihood.d3f(y, f[:, k], *_lpars)
-            H[:, k] = d2f[:, k].dot(Phi2) - 1. / _reg
+            H[:, k] = d2f[:, k].dot(Phi2) - 1. * ireg
 
             # Posterior mean and covariance gradients
             mkmj = _m[:, k][:, np.newaxis] - _m
@@ -182,7 +182,7 @@ def learn(X, y, likelihood, lparams, basis, bparams, regulariser=1.,
             dm[:, k] = (df[:, k].dot(Phi)
                         + 0.5 * _C[:, k] * d3f[:, k].dot(Phi3)
                         + (iCkCj * mkmj).dot(pz[:, k])
-                        - _m[:, k] / _reg) / K
+                        - _m[:, k] * ireg) / K
 
             # Likelihood parameter gradients
             for l, (dp, dp2df) in enumerate(zip(*lgrads(y, f[:, k], *_lpars))):
@@ -190,7 +190,7 @@ def learn(X, y, likelihood, lparams, basis, bparams, regulariser=1.,
                                + 0.5 * (_C[:, k] * dp2df.dot(Phi2)).sum()) / K
 
         # Regulariser gradient
-        dreg = (((_m**2).sum() + _C.sum()) / _reg**2 - D * K / _reg) / (2 * K)
+        dreg = (((_m**2).sum() + _C.sum()) * ireg**2 - D * K * ireg) / (2 * K)
 
         # Basis function parameter gradients
         def dtheta(dPhi):
@@ -208,7 +208,7 @@ def learn(X, y, likelihood, lparams, basis, bparams, regulariser=1.,
         # Objective, Eq. 10 in [1]
         L2 = 1. / K * (ll
                        - 0.5 * D * K * np.log(2 * np.pi * _reg)
-                       - 0.5 * (_m**2).sum() / _reg
+                       - 0.5 * (_m**2).sum() * ireg
                        + 0.5 * (_C * H).sum()
                        - logqk.sum() + np.log(K))
 

revrand/likelihoods.py
@@ -7,10 +7,10 @@ from __future__ import division
 
 import numpy as np
 from scipy.stats import bernoulli, poisson, norm
-from scipy.special import gammaln
+from scipy.special import gammaln, expit
 
 from .optimize import Positive
-from .transforms import logistic, softplus
+from .utils import safesoftplus, safediv, softplus
 
 
 #
@@ -19,8 +19,6 @@ from .transforms import logistic, softplus
 
 tiny = np.finfo(float).tiny
 logtiny = np.log(tiny)
-small = 1e-100
-resol = np.finfo(float).resolution
 
 
 #
@@ -78,7 +76,7 @@ class Bernoulli():
                 likelihood.
         """
 
-        ll = bernoulli.logpmf(y, logistic(f))
+        ll = bernoulli.logpmf(y, expit(f))
         ll[np.isinf(ll)] = logtiny
         return ll
 
@@ -97,7 +95,7 @@ class Bernoulli():
                 expected value of y, :math:`\mathbb{E}[y|f]`.
         """
 
-        return logistic(f)
+        return expit(f)
 
     def df(self, y, f):
         """
@@ -117,7 +115,7 @@ class Bernoulli():
                 the derivative :math:`\partial \log p(y|f) / \partial f`
         """
 
-        return y - logistic(f)
+        return y - expit(f)
 
     def d2f(self, y, f):
         """
@@ -138,7 +136,7 @@ class Bernoulli():
                 :math:`\partial^2 \log p(y|f)/ \partial f^2`
         """
 
-        sig = logistic(f)
+        sig = expit(f)
         return (sig - 1) * sig
 
     def d3f(self, y, f):
@@ -160,7 +158,7 @@ class Bernoulli():
                 :math:`\partial^3 \log p(y|f)/ \partial f^3`
         """
 
-        sig = logistic(f)
+        sig = expit(f)
         return (2 * sig - 1) * (1 - sig) * sig
 
     def dp(self, y, f):
@@ -229,7 +227,7 @@ class Bernoulli():
                 Cumulative density function evaluated at y.
         """
 
-        return bernoulli.cdf(y, logistic(f))
+        return bernoulli.cdf(y, expit(f))
 
 
 class Gaussian(Bernoulli):
@@ -314,7 +312,7 @@ class Gaussian(Bernoulli):
                 the derivative :math:`\partial \log p(y|f) / \partial f`
         """
 
-        return (y - f) / var
+        return safediv(y - f, var)
 
     def d2f(self, y, f, var):
         """
@@ -337,7 +335,7 @@ class Gaussian(Bernoulli):
                 :math:`\partial^2 \log p(y|f)/ \partial f^2`
         """
 
-        return - np.ones_like(f) / var
+        return - safediv(np.ones_like(f), var)
 
     def d3f(self, y, f, var):
         """
@@ -385,7 +383,7 @@ class Gaussian(Bernoulli):
                 where :math:`sigma^2` is the variance.
         """
 
-        return 0.5 * (((y - f) / var)**2 - 1. / var)
+        return 0.5 * (safediv(y - f, var)**2 - safediv(1., var))
 
     def dpd2f(self, y, f, var):
         """
@@ -410,7 +408,7 @@ class Gaussian(Bernoulli):
                 :math:`\\sigma^2`.
         """
 
-        return np.ones_like(f) / var**2
+        return safediv(np.ones_like(f), var**2)
 
     def cdf(self, y, f, var):
         """
@@ -522,7 +520,7 @@ class Poisson(Bernoulli):
         if self.tranfcn == 'exp':
             return y - np.exp(f)
         else:
-            return logistic(f) * (y / _safesoftplus(f) - 1)
+            return expit(f) * (y / safesoftplus(f) - 1)
 
     def d2f(self, y, f):
         """
@@ -546,8 +544,8 @@ class Poisson(Bernoulli):
         if self.tranfcn == 'exp':
             return - np.exp(f)
         else:
-            g = _safesoftplus(f)
-            gp = logistic(f)
+            g = safesoftplus(f)
+            gp = expit(f)
             g2p = gp * (1 - gp)
             return (y - g) * g2p / g - y * (gp / g)**2
 
@@ -573,8 +571,8 @@ class Poisson(Bernoulli):
         if self.tranfcn == 'exp':
             return self.d2f(y, f)
         else:
-            g = _safesoftplus(f)
-            gp = logistic(f)
+            g = safesoftplus(f)
+            gp = expit(f)
             g2p = gp * (1 - gp)
             g3p = g2p * (1 - 2 * gp)
             return g3p * (y - g) / g + 2 * y * (gp / g)**3 \
@@ -600,21 +598,3 @@ class Poisson(Bernoulli):
 
         mu = np.exp(f) if self.tranfcn == 'exp' else softplus(f)
         return poisson.cdf(y, mu=mu)
-
-
-#
-# Safe numerical operations
-#
-
-def _safelog(x):
-
-    cx = x.copy()
-    cx[cx < tiny] = tiny
-    return np.log(cx)
-
-
-def _safesoftplus(x):
-
-    g = softplus(x)
-    g[g < small] = small  # hack to avoid instability
-    return g

revrand/regression.py
@@ -18,7 +18,7 @@ import logging
 from scipy.optimize import minimize
 from scipy.linalg import cho_solve
 
-from .utils import append_or_extend
+from .utils import append_or_extend, safediv
 from .linalg import jitchol, cho_log_det
 from .optimize import Positive, structured_minimizer, logtrick_minimizer
 from .basis_functions import apply_grad
@@ -90,15 +90,19 @@ def learn(X, y, basis, bparams, var=1., regulariser=1., diagcov=False,
         Phi = basis(X, *_theta)                      # N x D
         PhiPhi = Phi.T.dot(Phi)
 
+        # Safe divide
+        ivar = safediv(1., _var)
+        ilambda = safediv(1., _lambda)
+
         # Posterior Parameters
         lower = False
-        LiC = jitchol(np.diag(np.ones(D) / _lambda) + PhiPhi / _var,
+        LiC = jitchol(np.diag(np.ones(D) * ilambda) + PhiPhi * ivar,
                       lower=lower)
-        m = cho_solve((LiC, lower), Phi.T.dot(y)) / _var
+        m = cho_solve((LiC, lower), Phi.T.dot(y)) * ivar
 
         # Common calcs dependent on form of C
         if diagcov:
-            C = 1. / (PhiPhi.diagonal() / _var + 1. / _lambda)
+            C = 1. / (PhiPhi.diagonal() * ivar + ilambda)
             TrPhiPhiC = (PhiPhi.diagonal() * C).sum()
             logdetC = np.log(C).sum()
             TrC = C.sum()
@@ -115,9 +119,9 @@ def learn(X, y, basis, bparams, var=1., regulariser=1., diagcov=False,
 
         # Calculate ELBO
         ELBO = -0.5 * (N * np.log(2 * np.pi * _var)
-                       + sqErr / _var
-                       + TrPhiPhiC / _var
-                       + (TrC + mm) / _lambda
+                       + sqErr * ivar
+                       + TrPhiPhiC * ivar
+                       + (TrC + mm) * ilambda
                        - logdetC
                        + D * np.log(_lambda)
                        - D)
@@ -137,15 +141,15 @@ def learn(X, y, basis, bparams, var=1., regulariser=1., diagcov=False,
                      .format(ELBO, _var, _lambda, _theta))
 
         # Grad var
-        dvar = 0.5 / _var * (-N + (sqErr + TrPhiPhiC) / _var)
+        dvar = 0.5 * ivar * (-N + (sqErr + TrPhiPhiC) * ivar)
 
         # Grad reg
-        dlambda = 0.5 / _lambda * ((TrC + mm) / _lambda - D)
+        dlambda = 0.5 * ilambda * ((TrC + mm) * ilambda - D)
 
         # Get structured basis function gradients
         def dtheta(dPhi):
             dPhiPhi = (dPhi * Phi).sum(axis=0) if diagcov else dPhi.T.dot(Phi)
-            return - (m.T.dot(Err.dot(dPhi)) - (dPhiPhi * C).sum()) / _var
+            return - (m.T.dot(Err.dot(dPhi)) - (dPhiPhi * C).sum()) * ivar
 
         dtheta = apply_grad(dtheta, basis.grad(X, *_theta))
 

revrand/utils/math.py
@@ -1,7 +1,21 @@
-""" Various nonlinear transformation functions. """
+""" Safe numerical operations. """
+
+from __future__ import division
 
 import numpy as np
 
+#
+# Module constants
+#
+
+eps = np.finfo(float).eps
+tiny = np.finfo(float).tiny
+small = 1e-100
+
+
+#
+# Special functions
+#
 
 def logsumexp(X, axis=0):
     """
@@ -33,41 +47,6 @@ def logsumexp(X, axis=0):
         mx = np.atleast_2d(mx).T if axis == 1 else np.atleast_2d(mx)
 
     return np.log(np.exp(X - mx).sum(axis=axis)) + np.ravel(mx)
-
-
-def logistic(X):
-    """
-    Pass X through a logistic sigmoid in a numerically stable way (using the
-    log-sum-exp trick).
-
-    The logistic sigmoid is
-
-    .. math::
-        \\frac{1}{1 + \exp\{-X\}}
-
-    Parameters
-    ----------
-        X: ndarray
-            (N,) array or shape (N, D) array of data.
-
-    Returns
-    -------
-        sig: ndarray
-            same shape of X with the result of logistic(X).
-    """
-
-    N = X.shape[0]
-
-    if X.ndim == 1:
-        return np.exp(-logsumexp(np.vstack((np.zeros(N), -X)).T, axis=1))
-    elif X.ndim == 2:
-        lgX = np.empty(X.shape, dtype=float)
-        for d in range(X.shape[1]):
-            lgX[:, d] = np.exp(-logsumexp(np.vstack((np.zeros(N),
-                                                     -X[:, d])).T, axis=1))
-        return lgX
-    else:
-        raise ValueError("This only works on up to 2D arrays.")
 
 
 def softmax(X, axis=0):
@@ -141,3 +120,29 @@ def softplus(X):
         return sftX
     else:
         raise ValueError("This only works on up to 2D arrays.")
+
+
+#
+# Numerically "safe" functions 
+#
+
+def safediv(num, den, min_den=eps):
+
+    if np.isscalar(den):
+        return num / max(den, min_den)
+    else:
+        return num / np.fmax(den, min_den)
+
+
+def safelog(x, min_x=tiny):
+
+    cx = x.copy()
+    cx[cx < min_x] = min_x
+    return np.log(cx)
+
+
+def safesoftplus(x, min_x=small):
+
+    g = softplus(x)
+    g[g < min_x] = min_x
+    return g

tests/test_utils.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 import numpy as np
 
-import revrand.transforms as tfms
+import revrand.utils.math as tfms
 
 
 def test_logsumexp():
@@ -33,20 +33,6 @@ def test_softmax():
     # Axis 1
     sma1 = tfms.softmax(X, axis=1)
     assert np.allclose(sma1.sum(axis=1), np.ones(10))
-
-
-def test_logistic():
-
-    # Function validity test
-    X = np.random.randn(10, 3)
-    sig_actual = 1 / (1 + np.exp(-X))
-    sig_test = tfms.logistic(X)
-
-    assert np.allclose(sig_actual, sig_test)
-
-    # Extreme value tests
-    assert np.allclose(tfms.logistic(np.array([1e3])), np.ones(1))
-    assert np.allclose(tfms.logistic(np.array([-1e3])), np.zeros(1))
 
 
 def test_sofplus():