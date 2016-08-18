2de90ea82ae7933f8b3de16d332e842140c18c96
Daniel Steinberg <dan.m.steinberg@gmail.com>
2016-08-18 12:35:22 +1000

    udpated version no

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class PyTest(TestCommand, object):
 
 setup(
     name='revrand',
-    version='0.6.1',
+    version='0.6.2',
     description='A library of scalable Bayesian generalised linear models with'
                 ' fancy features',
     author='Daniel Steinberg',
